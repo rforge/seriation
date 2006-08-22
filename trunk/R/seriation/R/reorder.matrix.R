afd798a15f0475b6b375a0fa387ceac55dc13ae6
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-08-22 15:07:10 +0000

    moved stuff to tsp.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seriation@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/seriation/R/reorder.matrix.R
+++ trunk/R/seriation/R/reorder.matrix.R
@@ -145,52 +145,7 @@ reorder.matrix <- function(x, method = NULL, row = TRUE, ...) {
         stop(paste("Methods",sQuote("nearest/farthest insertion"),
                 "require a symmetrical distance matrix"))
     
-    n <- nrow(x)
-    m <- ncol(x)
-
-    if (start == 0) start <- sample(1:n, 1)
-
-    placed <- logical(n)
-    placed[start] <- TRUE
-    order <- c(start)
-   
-    while(any(placed == FALSE)) {
-        # find city j (in tour) and city k (not jet used) which are closest
-        js <- which(placed)
-        ks <- which(!placed)
-
-        # which.min does not break ties by random!
-        if(nearest == TRUE) mx <- which.min(x[js,ks, drop = FALSE])
-        else mx <- which.max(x[js,ks, drop = FALSE])
-        k <- ks[(mx-1) %/% length(js) + 1]
-        j <- js[(mx-1) %% length(js) + 1]
-
-        # now we do nearest insertion
-        placed[k] <- TRUE
-        if(length(order) == 1) order <- c(order, k)
-        else {
-            bestVal <- Inf
-            insert <- 0
-            for(i in 1:(length(order)-1)) {
-                val <- x[order[i], k] + x[k, order[i+1]] - x[order[i], order[i+1]]
-                if(val < bestVal) {
-                    bestVal <- val
-                    insert <- i
-                }
-            }
-            
-            # now between the last an first city
-            val <- x[order[length(order)], k] + x[k,order[1]] 
-                - x[order[length(order)],order[1]]
-            if(val < bestVal) {
-                bestVal <- val
-                insert <- 0     # we just append k
-            }
-
-            if(insert == 0) order <- c(order, k)
-            else order <- c(order[1:insert], k, order[(insert+1):length(order)]) 
-        }
-    }
+    order <- tsp_insertion(as.dist(x), nearest = nearest, start = start)
 
     # To find the cutting point in the tour, typically a dummy city with
     # equal distance to every other city is added. The dummy city is then
