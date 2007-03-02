db7fd4bd9184415594eddcd126f361fafbe663f9
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-03-02 13:03:36 +0000

    ...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seriation@689 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/seriation/R/bertinplot.R
+++ trunk/seriation/R/bertinplot.R
@@ -5,9 +5,14 @@ bertinplot  <- function(x, highlight = NULL, options = NULL) {
     ## default plot options
     user_options <- options
     options <- list(
-        barwidth    = 0.8,
+        panel_function     = panel.bars, 
+        reverse     = FALSE,
+        xlab        = NULL,
+        ylab        = NULL,
+        spacing     = 0.2,
         mar         = c(5, 4, 8, 8),
-        cex         = 0.9,
+        gp_labels   = gpar(cex = 0.9),
+        #        gp_panel    = gpar(),
         newpage     = TRUE,
         pop         = TRUE
     )
@@ -19,55 +24,95 @@ bertinplot  <- function(x, highlight = NULL, options = NULL) {
         if(any(is.na(o))) stop(paste("Unknown plot option:",
                 names(user_options)[is.na(o)], "\n\t"))
 
-        for (i in 1:length(o)) {
-            options[[o[i]]] <- user_options[[i]]
-        }
+        for (i in 1:length(o)) options[[o[i]]] <- user_options[[i]]
+    }
+    
+    attach(options)
+    on.exit(detach("options"))
+    
+    do_hl <- FALSE
+    if(is.null(highlight)) { 
+        do_hl <- TRUE
+        highlight <- matrix(NA, ncol = ncol(x), nrow = nrow(x))
+    } 
+
+    for (variable in 1:ncol(x)) { 
+        ## scaling
+        x[,variable] <- x[,variable]/max(x[,variable], na.rm = TRUE)
+
+        ## highlight
+        if(do_hl) 
+        highlight[,variable] <- x[,variable] > mean(x[,variable], na.rm = TRUE)
     }
 
-    ncases <- dim(x)[1]
-    nvariables <- dim(x)[2]
-    barwidth <- options$barwidth
-    xlim <- c(1 - barwidth, ncases + barwidth)
-    
+
+    ## note: Bertin switched cols and rows for his display!
+    if(reverse) {
+        x <- t(x)
+        highlight <- t(highlight)
+    }
+
+    ncol_x  <- ncol(x)
+   
     ## clear page
-    if(options$newpage) grid.newpage()
+    if(newpage) grid.newpage()
  
-    #c(bottom, left, top, right)
-    pushViewport(plotViewport(margins = options$mar,
-            layout = grid.layout(nvariables, 1), 
-            xscale = xlim, yscale = c(1,nvariables), default.unit = "native"))
+    ## create outer viewport
+    xlim <- c(1 - (1 - spacing), nrow(x) + (1 - spacing))
+    pushViewport(plotViewport(margins = mar,
+            layout = grid.layout(ncol_x, 1), 
+            xscale = xlim, 
+            yscale = c(1, ncol_x), default.unit = "native"))
 
-    for (variable in 1:nvariables) { 
-        height <- x[, variable]
-
-        hl <- if(is.null(highlight)) height > mean(height, na.rm = TRUE) 
-        else highlight[, variable]
+    for (variable in 1:ncol_x) { 
+        value <- x[, variable]
+        hl <- highlight[, variable]
 
         pushViewport(viewport(layout.pos.col = 1, layout.pos.row = variable,
-                xscale = xlim, yscale = c(0, max(height, na.rm = TRUE) * 1.1), 
+                xscale = xlim, 
+                yscale = c(0, 1 + spacing), 
                 default.unit = "native"))
 
-        grid.rect(x = 1:ncases, y = 0, width = barwidth, height = height,
-            just = c("centre", "bottom"), default.units = "native", 
-            gp = gpar(fill = hl))
+        ## call panel function
+        panel_function(value, spacing, hl)
 
         upViewport(1)
     }
 
-    cases <- labels(x)[[1]]
-    variables <- labels(x)[[2]]
+    ## do labels
+    rownames_x <- if(is.null(xlab)) rownames(x) else xlab
+    colnames_x <- if(is.null(ylab)) colnames(x) else ylab
 
-    grid.text(cases, x = 1:ncases, y = nvariables, 
+    grid.text(rownames_x, x = 1:nrow(x), y = ncol_x, 
         rot = 90, just = "left",
-        default.units= "native", gp = gpar(cex = options$cex))
+        default.units= "native", gp = gp_labels)
 
-    grid.text(rev(variables), x = 1, 
-        y = 0.5:(nvariables-0.5)/nvariables,
+    grid.text(rev(colnames_x), x = 1, 
+        y = 0.5:(ncol_x-0.5)/ncol_x,
         just = "left", 
-        default.units= "npc", gp = gpar(cex = options$cex))
+        default.units= "npc", gp = gp_labels)
 
-    if (options$pop == TRUE) popViewport(1)
+    if (pop == TRUE) popViewport(1)
     else upViewport(1)
 
 }
 
+## panel functions
+panel.bars <- function(value, spacing, hl) {
+    grid.rect(x = 1:length(value), y = 0, width = 1 - spacing, height = value,
+        just = c("centre", "bottom"), default.units = "native", 
+        gp = gpar(fill = hl))
+}
+
+panel.circles <- function(value, spacing, hl) {
+    grid.circle(x = c(1:length(value)), y = unit(0.5, "npc"), 
+        r = value/2*(1 - spacing),
+        default.units = "native", 
+        gp = gpar(fill = hl))
+}
+
+
+panel.lines <- function(value, spacing, hl) {
+    grid.lines(x = c(1:length(value)), y = value*(1-spacing), 
+        default.units = "native")
+}
