961b343c85ffb8d11ee9933fbfef7b7d79fdb3f7
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-01 02:20:58 +0000

    added shading and panel.blocks to bertinplot().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seriation@782 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bertinplot.R
+++ pkg/R/bertinplot.R
@@ -45,6 +45,7 @@ function(x, order = NULL, highlight = TRUE, options = NULL)
         mar         = c(5, 4, 8, 8),
         gp_labels   = gpar(),
         gp_panels   = gpar(),
+	shading	    = FALSE,
         newpage     = TRUE,
         pop         = TRUE
     )
@@ -63,21 +64,45 @@ function(x, order = NULL, highlight = TRUE, options = NULL)
         options[o] <- user_options
     }
     
-    ## scale each variable in x for plotting (between 0 and 1)
-    x <- x/ matrix(apply(abs(x), 2, max, na.rm = TRUE), 
-        byrow= TRUE, ncol=ncol(x), nrow= nrow(x))
-   
+    ## panel.blocks has no spacing!
+    if(identical(options$panel.function, panel.blocks)) {
+	options$spacing <- 0
+    }
+    
+    ## scale each variable in x for plotting (between 0 and 1 or -1 and 1)
+    ## this can deal with 0s, na, nan, but plots inf as na
+    infs <- is.infinite(x)
+    infsign <- sign(x[is.infinite(x)])
+    
+    scalem <-matrix(apply(abs(x), 2, max, na.rm = TRUE),
+	            byrow= TRUE, ncol=ncol(x), nrow= nrow(x))
+    scalem[scalem==0] <- 1	
+    
+    x <- x/scalem 
+    
+    if(any(infs)) x[infs] <- infsign
+
+    # x <- x/ matrix(apply(abs(x), 2, max, na.rm = TRUE), 
+    #    byrow= TRUE, ncol=ncol(x), nrow= nrow(x))
     ## fix division by zero (if all entries in a row are 0)
-    x[is.nan(x)] <- 0
+    #x[is.nan(x)] <- 0
 
     ## highlight
     if(length(highlight) == 1 && highlight) 
-        highlight <- x > rowMeans(x, na.rm = TRUE)
+        highlight <- x > matrix(colMeans(x, na.rm = TRUE), 
+	    ncol=ncol(x), nrow=nrow(x), byrow=TRUE)
     else if(length(highlight) == 1 && !highlight)
         highlight <- matrix(FALSE, ncol = ncol(x), nrow = nrow(x))
     else if(any(dim(x) != dim(highlight)))
         stop("Argument 'highlight' has incorrect dimensions.")
 
+    ## shading?
+    if(options$shading) {
+	highlight <- map(x, c(.8,.1))
+	highlight[!is.finite(highlight)] <- 1
+	highlight <- matrix(gray(highlight), ncol=ncol(x), nrow=nrow(x))
+    }
+    
     ## note: Bertin switched cols and rows for his display!
     if(options$reverse) {
         x <- t(x)
@@ -85,25 +110,29 @@ function(x, order = NULL, highlight = TRUE, options = NULL)
     }
 
     ncol_x  <- ncol(x)
+	
    
     ## clear page
     if(options$newpage) grid.newpage()
  
     ## create outer viewport
-    xlim <- c(options$spacing, nrow(x) + (1 - options$spacing))
-    pushViewport(plotViewport(margins = options$mar,
-            layout = grid.layout(ncol_x, 1), 
-            xscale = xlim, 
-            yscale = c(1, ncol_x), default.units = "native"))
+    xlim <- c(options$spacing, nrow(x) + 1 - options$spacing)
+    pushViewport(plotViewport(margins = options$mar, 
+		    layout = grid.layout(ncol_x, 1), 
+		    xscale = xlim, 
+		    yscale = c(0, ncol_x), 
+		    default.units = "native", 
+		    name = "bertin"))
 
     for (variable in 1:ncol_x) { 
         value <- x[, variable]
         hl <- highlight[, variable]
 
+	## handle neg. values
 	if(identical(options$panel.function, panel.bars) ||
 		identical(options$panel.function, panel.lines)) {
 	    ylim <- c(min(value,0, na.rm=TRUE),
-		    max(value,0.1, na.rm=TRUE)) ## we use 0.1 so 0s show!
+		    max(value,0, na.rm=TRUE) + options$spacing) 
 	}else{
 	    ylim <- c(0,
 		    max(abs(value),0.1, na.rm=TRUE))
@@ -140,12 +169,13 @@ function(x, order = NULL, highlight = TRUE, options = NULL)
         popViewport(1)
     else
         upViewport(1)
-
 }
+
+
 
 ## panel functions
 panel.bars <- function(value, spacing, hl) {
-    grid.rect(x = 1:length(value), y = 0, 
+    grid.rect(x = 1:length(value), y = spacing/2, 
         width = 1 - spacing, 
         height = value*(1 - spacing),
         just = c("centre", "bottom"), default.units = "native", 
@@ -155,11 +185,11 @@ panel.bars <- function(value, spacing, hl) {
 panel.circles <- function(value, spacing, hl) {
     ## neg. values are dashed
     lty <- as.integer(value<0)+1L
-    ## do not plot zero circles
-    value[value == 0] <- NA
+    lty[!is.finite(lty)] <- 0L
 
+    value <- abs(value)
 
-    grid.circle(x = c(1:length(value)), 
+    grid.circle(x = 1:length(value), y=.5, 
         r = value/2*(1 - spacing),
         default.units = "native", 
 	gp = gpar(fill = hl, lty=lty))
@@ -168,17 +198,46 @@ panel.circles <- function(value, spacing, hl) {
 panel.squares <- function(value, spacing, hl) {
     ## neg. values are dashed
     lty <- as.integer(value<0)+1L
-    ## do not plot zero squares
-    value[value == 0] <- NA
+    lty[!is.finite(lty)] <- 0L
     
     grid.rect(x = 1:length(value), 
         width = value*(1 - spacing), 
         height = value*(1 - spacing),
         default.units = "native",
+	just = c("centre", "center"),
 	gp = gpar(fill = hl, lty=lty))
+}
+
+panel.blocks <- function(value, spacing, hl) {
+    grid.rect(x = 1:length(value), 
+        width = 1, 
+        height = unit(1, "npc"),
+        default.units = "native",
+	just = c("centre", "center"),
+	gp = gpar(fill = hl))
 }
 
 panel.lines <- function(value, spacing, hl) {
     grid.lines(x = c(1:length(value)), y = value*(1-spacing), 
         default.units = "native")
+}
+
+
+## add cut lines manually to a bertin plot 
+bertin_cut_line <- function(x = NULL, y = NULL) {
+    if(length(x) <2) x <- rep(x,2)
+    if(length(y) <2) y <- rep(y,2)
+    
+    seekViewport("bertin")
+   
+    if(is.null(x)) x <- unit(c(0,1), units="npc") 
+    else x <- x+.5
+    
+    if(is.null(y)) y <- unit(c(0,1), units="npc")
+    else y <- y
+
+    grid.lines(x = x,
+	    y = y,
+	    default.units= "native",
+	    gp=gpar(col="gray", lwd=2))
 }
---
+++ pkg/Work/Bertin_test/bertin_test.R
@@ -0,0 +1,79 @@
+## tests for Bertinplot provided by G. Sawitzki
+
+
+#random matrices
+nrow <- 5
+ncol <- 3
+
+BMunif <- matrix(runif(nrow*ncol), nrow, ncol)
+colnames(BMunif) <- colnames(BMunif, do.NULL=FALSE)
+rownames(BMunif) <- rownames(BMunif, do.NULL=FALSE)
+
+bertinplot(BMunif)
+
+
+BMnorm <- matrix(rnorm(nrow*ncol), nrow, ncol)
+colnames(BMnorm) <- colnames(BMnorm, do.NULL=FALSE)
+rownames(BMnorm) <- rownames(BMnorm, do.NULL=FALSE)
+bertinplot(BMnorm)
+bertinplot(BMnorm, options=list(panel=panel.circles))
+
+bertinplot(BMnorm, options=list(panel=panel.circles, reverse=TRUE))
+bertinplot(BMnorm, options=list(shading=TRUE))
+bertinplot(BMnorm, options=list(shading=TRUE, panel=panel.blocks))
+bertinplot(BMnorm, highlight=BMnorm>1)
+
+Nrcases <- 7
+
+# Test vectors, used to build a matrix
+Bzero <- rep(0, Nrcases)
+Bone <- rep(1, Nrcases)
+Bmone <- rep(-1, Nrcases)
+Binc <- (1:Nrcases)
+Bdec <- (Nrcases:1)
+Bstep <- c(Bmone[Binc < Nrcases/2], Bone[Binc >= Nrcases/2])
+Bhat <- Bone; Bhat[(floor(Nrcases/3)+1):(Nrcases-floor(Nrcases/3)) ] <- 0.5
+Bnazero <- rep(c(NA,0),length.out= Nrcases)
+Bnanzero <- rep(c(NaN,0),length.out= Nrcases)
+Binf <- rep(c(Inf,10,0,-10,-Inf),length.out= Nrcases)
+
+# Basic test matrices
+Brmatrix <- rbind(Bzero, Bone, Bmone, Binc, Bdec, Bstep, Bhat)
+colnames(Brmatrix) <- colnames(Brmatrix,FALSE)
+
+bertinplot(Brmatrix)
+bertinplot(Brmatrix, options=list(panel=panel.circles))
+## FIXME: this one fails!!!
+bertinplot(Brmatrix, options=list(panel=panel.circles, reverse=TRUE))
+
+## R may use internal housekeeping to keep matrix columns hogogeneous. Check!
+## Use row matrix and column matrix for tests.
+Bcmatrix <- cbind(Bzero, Bone, Bmone, Binc, Bdec, Bstep, Bhat)
+rownames(Bcmatrix) <- rownames(Bcmatrix,FALSE)
+
+bertinplot(Bcmatrix)
+
+# Basic test matrices with random error
+BrRndmatrix <- Brmatrix+rnorm(nrow(Brmatrix)*ncol(Brmatrix))
+
+bertinplot(BrRndmatrix)
+
+
+# Test matrices with IEEE specials
+Brmatrixx <- rbind(Bzero, Bone, Bmone, Binc, Bdec, Bstep, Bhat, Bnazero,
+	Bnanzero, Binf)
+
+bertinplot(Brmatrixx)
+bertinplot(Brmatrixx, options=list(shading=TRUE))
+bertinplot(Brmatrixx, options=list(reverse=TRUE))
+bertinplot(Brmatrixx, options=list(panel=panel.squares))
+
+## FIXME: this one fails!!!
+bertinplot(Brmatrixx, options=list(panel=panel.circles, reverse=TRUE))
+
+Bcmatrixx <- cbind(Bzero, Bone, Bmone, Binc, Bdec, Bstep, Bhat, Bnazero,
+	Bnanzero,Binf)
+
+bertinplot(Bcmatrixx)
+
+
